c7fa1976d57bb310e2ef54890bbdb22e962f81cc
Abdul <wbehzad12@gmail.com>
2021-07-16 11:03:14 -0500

    fixed insertion sort implementation

--- naive_sorting_algos/insertion_sort.js
+++ naive_sorting_algos/insertion_sort.js
@@ -2,6 +2,11 @@ function insertionSort(array) {
   for (let i = 1; i < array.length; i++) {
     let curElement = array[i];
 
-    for (let j = i - 1; j >= 0 && curElement < array[i]; j++) {}
+    for (let j = i - 1; j >= 0 && curElement < array[j]; j--) {
+      array[j + 1] = array[j];
+    }
+    array[j + 1] = curElement;
   }
+
+  return array;
 }
